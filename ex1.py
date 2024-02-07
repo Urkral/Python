52c8d66d0a84e5ef5a9a704d6a66a71cd884b992
Urkral <esteban.dutrannois@outlook.com>
2024-02-07 11:31:58 +0100

    -

---
+++ ex1.py
@@ -0,0 +1,8 @@
+isDepreter = input("Qui est votre professeur préféré ?")
+
+if isDepreter == "DEPRETER":
+    print('Oui - Mr Depreter est le meilleur professeur de tous les temps! ')
+elif isDepreter == "depreter":
+    print('Non, je veux le vrai Mr Depreter!')
+else:
+    print('Mr Depreter! Pas ' + isDepreter )
---
+++ ex2.py
@@ -0,0 +1,31 @@
+# import time
+# for t in range(5):
+#   time.sleep(1)
+#   print(t + 1, "Mississipi")
+# time.sleep(1)
+# print('Prêt ou pas prêt j''arrive') 
+
+
+
+
+# -----------------------------------------------------------------------------------------
+
+# word = str
+# while (word != "capuera"):
+#     word = input('Entrez un mot !')
+#     goodword = "capuera"
+#     if (word == goodword):
+#         print('Bien ouej pelo tu es sorti de la boucle')
+#         break
+
+# -----------------------------------------------------------------------------------------
+
+# userWord = input('Entre votre mot !')
+# userWord.upper
+# for vL in userWord:
+#     if(vL != "o" and vL !="u" and vL != 'a' and vL != 'e' and vL !='i'):
+#         print(vL)
+
+# -----------------------------------------------------------------------------------------
+
+
